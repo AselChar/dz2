b196fdb5c8a8fe696a1a16d91a4f70224f5b32f5
AselChar <charabaevaa@mail.ru>
2023-09-05 14:22:01 +0600

    dz2

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import {useState} from "react";
+
+function App() {
+    const [color1 , setColor1]=useState("blue")
+    const [color2 , setColor2]=useState("red")
+
+
+    const SwapColor = ()=>{
+        setColor1(color2)
+        setColor2(color1)
+    }
+
+
+
+return (
+    <div>
+        <div className="Square"
+            onClick={SwapColor}
+             style={{backgroundColor:color2}}></div>
+        <div className="Square_2"
+            onClick={SwapColor}
+        style={{backgroundColor:color1}}></div>
+
+    </div>
+)
+}
+export default App
